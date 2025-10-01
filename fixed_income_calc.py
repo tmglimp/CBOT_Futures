75a3d7b690c65cefb354a287a98d9f3138033c08
T. Madison Glimp <100633051+tmglimp@users.noreply.github.com>
2025-09-30 20:20:59 -0600

    Update fixed_income_calc.py

--- fixed_income_calc.py
+++ fixed_income_calc.py
@@ -412,22 +412,6 @@ def fut_sensitivityMKTminus(cpn, term, yield_, period=2, begin=None, settle=None
         return None
     return round((mdur) * 0.001, 6)/conv_factor
 
-def fut_tail(A_FUT_DV01, A_MULT, B_FUT_DV01, B_MULT):
-    return (-1 *
-        (((A_FUT_DV01 * A_MULT) - (B_FUT_DV01 * B_MULT)) / (B_FUT_DV01 * B_MULT))
-        if (A_FUT_DV01 * A_MULT) > (B_FUT_DV01 * B_MULT)
-        else (((B_FUT_DV01 * B_MULT) - (A_FUT_DV01 * A_MULT)) / (A_FUT_DV01 * A_MULT))
-    )
-
-def fwd_fut_tail(A_FUT_DV01, A_FWD_DV01, A_MULT, B_FUT_DV01, B_FWD_DV01, B_MULT):
-    return (-1 *
-        ((((A_FUT_DV01+A_FWD_DV01) * A_MULT) - ((B_FUT_DV01+B_FWD_DV01) * B_MULT)) /
-                ((B_FUT_DV01+B_FWD_DV01) * B_MULT))
-        if (A_FUT_DV01 * A_MULT) > (B_FUT_DV01 * B_MULT)
-        else ((((B_FUT_DV01+B_FWD_DV01) * B_MULT) - ((A_FUT_DV01+A_FWD_DV01) * A_MULT)) /
-                ((A_FUT_DV01+A_FWD_DV01) * A_MULT))
-    )
-
 def appx_duration(cpn, term, yield_, period=2, begin=None, settle=None, next_coupon=None, day_count=1,delta_y=0.0001):
     if yield_ is None:
         return None
