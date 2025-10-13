259302805f686109ce5e494ec524db908e891cdc
Thomas Madison Glimp <100633051+tmglimp@users.noreply.github.com>
2025-10-13 13:12:25 -0600

    Update fixed_income_calc.py

--- fixed_income_calc.py
+++ fixed_income_calc.py
@@ -420,3 +420,19 @@ def appx_convexity(cpn, term, yield_, period=2, begin=None, settle=None, next_co
     if price is None or price == 0:
         return None
     return (p_bp_up + p_bp_dn - 2 * price) / (price * delta_y ** 2)
+
+def fut_tail(A_FUT_DV01, A_MULT, B_FUT_DV01, B_MULT):
+    return ( -1 *
+    (((A_FUT_DV01 * A_MULT) - (B_FUT_DV01 * B_MULT)) / (B_FUT_DV01 * B_MULT))
+    if (A_FUT_DV01 * A_MULT) > (B_FUT_DV01 * B_MULT)
+    else (((B_FUT_DV01 * B_MULT) - (A_FUT_DV01 * A_MULT)) / (A_FUT_DV01 * A_MULT))
+    )
+
+def fwd_fut_tail(A_FUT_DV01, A_FWD_DV01, A_MULT, B_FUT_DV01, B_FWD_DV01, B_MULT):
+    return (-1 *
+    ((((A_FUT_DV01+A_FWD_DV01) * A_MULT) - ((B_FUT_DV01+B_FWD_DV01) * B_MULT)) /
+    ((B_FUT_DV01+B_FWD_DV01) * B_MULT))
+    if (A_FUT_DV01 * A_MULT) > (B_FUT_DV01 * B_MULT)
+    else ((((B_FUT_DV01+B_FWD_DV01) * B_MULT) - ((A_FUT_DV01+A_FWD_DV01) * A_MULT)) /
+    ((A_FUT_DV01+A_FWD_DV01) * A_MULT))
+    )
